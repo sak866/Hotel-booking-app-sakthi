984bb1d7d86431dffc0d988b50bb1f743ac1d8fc
sak866 <ramyakumar701@gmail.com>
2026-02-17 15:16:00 +0530

    Add files via upload

---
+++ HotelBookingSakthiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingSakthiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingSakthiApplication.class, args);
+	}
+
+}
